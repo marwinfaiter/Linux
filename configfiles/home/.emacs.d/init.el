2e6274bb929eba33ca25baa49a2c4b0ad0b17299
Martin Falkmer <noobgubbe@live.se>
2020-02-24 21:40:15 +0100

    Added emacs config files

---
+++ configfiles/home/.emacs.d/init.el
@@ -0,0 +1,34 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/emacsinit.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (doom-molokai)))
+ '(custom-safe-themes
+   (quote
+    ("be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" default)))
+ '(ediff-diff-options "-w")
+ '(ediff-split-window-function (quote split-window-horizontally))
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(package-selected-packages
+   (quote
+    (aggressive-indent smartparens evil-nerd-commenter highlight-parentheses rainbow-delimiters neotree ranger zenburn-theme which-key use-package try powerline ox-reveal orgalist org-bullets molokai-theme magit gnu-elpa-keyring-update eww-lnum evil doom-themes counsel auto-complete ace-window abyss-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
