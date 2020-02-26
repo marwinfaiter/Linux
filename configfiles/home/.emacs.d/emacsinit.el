038d2d96aebbad01111008f7a3e1cda2d4d86ed1
Martin Falkmer <noobgubbe@live.se>
2020-02-26 01:29:58 +0100

    Updated emacsinit.org with more packages and config

--- configfiles/home/.emacs.d/emacsinit.el
+++ configfiles/home/.emacs.d/emacsinit.el
@@ -2,27 +2,82 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (fset 'yes-or-no-p 'y-or-n-p)
-(global-set-key (kbd "<f5>") 'revert-buffer)
 
 (setq make-backup-files nil)
-(use-package saveplace
-    :init (save-place-mode))
-    :ensure t
 (setq auto-save-default nil)
 
-(use-package which-key
-  :ensure t
-  :config
-  (which-key-mode))
+(customize-set-variable 'ad-redefinition-action 'accept)
 
-(use-package ox-reveal
+(require 'package)
+(setq package-check-signature nil)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+            '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+            '("org" . "https://orgmode.org/elpa/"))
+(package-initialize)
+
+(when (not package-archive-contents)
+(package-refresh-contents))
+
+(when (not (package-installed-p 'use-package))
+(package-install 'use-package))
+
+(require 'use-package)
+(customize-set-variable 'use-package-always-ensure t)
+(customize-set-variable 'use-package-verbose nil)
+
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+(use-package saveplace
 :ensure t)
+:config
+(save-place-mode)
 
-(setq org-reveal-root "http://cdn.jsdeliver.net/npm/reveal.js/")
-(setq org-reveal-mathjax t)
+(use-package smooth-scrolling
+:config
+(smooth-scrolling-mode 1))
 
-(use-package htmlize
-   :ensure t)
+(require 'bind-key)
+(bind-key (kbd "<f5>") 'revert-buffer)
+
+(use-package company
+:diminish company-mode
+:hook
+(after-init . global-company-mode))
+
+(setq completion-ignore-case t)
+(customize-set-variable 'read-file-name-completion-ignore-case t)
+(customize-set-variable 'read-buffer-completion-ignore-case t)
+
+(defalias 'list-buffers 'ibuffer)
+
+(use-package ace-window
+:ensure t
+:init
+(global-set-key [remap other-window] 'ace-window)
+(custom-set-faces
+'(aw-leading-char-face
+    ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+)
+
+(use-package eyebrowse
+:ensure t
+:config
+(eyebrowse-mode t)
+(eyebrowse-setup-opinionated-keys))
+
+(use-package cperl-mode
+:ensure t
+:mode "\\.p[lm]\\'"
+:interpreter "perl"
+:config
+(setq cperl-hairy t))
+
+(custom-set-variables
+'(ediff-diff-options "-w")
+'(ediff-split-window-function (quote split-window-horizontally))
+'(ediff-window-setup-function (quote ediff-setup-windows-plain)))
 
 (use-package evil
 :ensure t
@@ -61,26 +116,178 @@
 :init
 (doom-modeline-mode 1))
 
+(use-package evil-org
+:ensure t
+:after org
+:config
+(add-hook 'org-mode-hook 'evil-org-mode)
+(add-hook 'evil-org-mode-hook
+(lambda ()
+(evil-org-set-key-theme)))
+(require 'evil-org-agenda)
+(evil-org-agenda-set-keys))
+
+(use-package counsel
+    :ensure t)
+
+(use-package ido
+:ensure t
+:config
+(ido-mode t)
+(ido-everywhere 1)
+(setq ido-use-virtual-buffers t)
+(setq ido-enable-flex-matching t)
+(setq ido-use-filename-at-point nil)
+(setq ido-auto-merge-work-directories-length -1))
+
+(use-package ido-completing-read+
+:ensure t
+:config
+(ido-ubiquitous-mode 1))
+
+(use-package swiper
+:ensure t
+:config
+(defun custom-find-file ()
+"Uses projectile if in a git repo, otherwise ido"
+(interactive)
+(let ((project-dir (projectile-project-root)))
+(if project-dir
+(progn
+(projectile-find-file))
+(ido-find-file))))
+
+:bind
+(("C-s" . swiper)
+("C-r" . swiper)
+("C-c C-r" . ivy-resume)
+("M-x" . counsel-M-x)
+("C-x C-f" . custom-find-file))
+:config
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq ivy-display-style 'fancy)
+(define-key read-expression-map (kbd "C-r") 'counsel-expression-history))
+
+(use-package hl-line
+:ensure t
+:config
+(global-hl-line-mode))
+
+(use-package iedit
+:config
+(set-face-background 'iedit-occurrence "Magenta")
+:bind
+("C-x M-r" . iedit-mode))
+
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
 
-(define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)
+;(use-package auto-indent-mode
+;:ensure t
+;:config
+;(auto-indent-global-mode))
 
-(use-package auto-indent-mode
+(global-linum-mode)
+
+(use-package magit
+:diminish auto-revert-mode
+:bind
+(("C-x g" . magit-status)
+:map magit-status-mode-map
+("q"       . magit-quit-session))
+:config
+(defadvice magit-status (around magit-fullscreen activate)
+"Make magit-status run alone in a frame."
+(window-configuration-to-register :magit-fullscreen)
+ad-do-it
+(delete-other-windows))
+
+(defun magit-quit-session ()
+"Restore the previous window configuration and kill the magit buffer."
+(interactive)
+(kill-buffer)
+(jump-to-register :magit-fullscreen)))
+
+(use-package evil-multiedit
 :ensure t
 :config
-(auto-indent-global-mode))
+(evil-multiedit-default-keybinds))
 
-;(require 'autopair)
-;(autopair-global-mode) ;; enable autopair in all buffers
+(use-package neotree
+:ensure t
+:custom
+(neo-theme (if (display-graphic-p) 'icons 'arrow))
+(neo-smart-open t)
+(projectile-switch-project-action 'neotree-projectile-action)
+:config
+(setq-default neo-show-hidden-files t)
+(defun neotree-project-dir ()
+"Open NeoTree using the git root."
+(interactive)
+(let ((project-dir (projectile-project-root))
+(file-name (buffer-file-name)))
+(neotree-toggle)
+(if project-dir
+  (if (neo-global--window-exists-p)
+    (progn
+    (neotree-dir project-dir)
+    (neotree-find file-name)))
+    (message "Could not find git project root."))))
+:bind
+([f8] . neotree-project-dir))
+
+(setq require-final-newline t)
+
+(use-package org-bullets
+:after org
+:hook
+(org-mode . (lambda () (org-bullets-mode 1))))
+(defun zz/org-reformat-buffer ()
+(interactive)
+(when (y-or-n-p "Really format current buffer? ")
+(let ((document (org-element-interpret-data (org-element-parse-buffer))))
+(erase-buffer)
+(insert document)
+(goto-char (point-min)))))
+
+(use-package ox-reveal
+:ensure t)
+
+(setq org-reveal-root "http://cdn.jsdeliver.net/npm/reveal.js/")
+(setq org-reveal-mathjax t)
+
+(use-package htmlize
+:ensure t)
+
+(use-package toc-org
+:after org
+:hook
+(org-mode . toc-org-enable))
+
+(use-package paradox
+:custom
+(paradox-github-token t)
+:config
+(paradox-enable))
+
+(setq evil-normal-state-tag "NORMAL")
+(setq evil-insert-state-tag "INSERT")
+(setq evil-visual-state-tag "VISUAL")
+
+(use-package projectile
+:diminish projectile-mode
+:config
+(projectile-global-mode))
+
 (use-package smartparens-config
 :ensure smartparens
 :config
 (show-smartparens-global-mode t))
 
-(add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
-(add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode)
+(add-hook 'prog-mode-hook 'turn-on-smartparens-mode)
+(add-hook 'markdown-mode-hook 'turn-on-smartparens-mode)
 
 (use-package rainbow-delimiters
 :ensure t
@@ -89,90 +296,20 @@
 
 (show-paren-mode 1)
 
-(global-linum-mode)
-
-;(require 'org-tempo)
-(use-package org-bullets
-    :ensure t
-    :config
-    (add-hook 'org-mode-hook(lambda () (org-bullets-mode 1 ))))
-
-(evil-define-key 'normal org-mode-map (kbd "TAB") #'org-cycle)
-
-(custom-set-variables
- '(ediff-diff-options "-w")
-    '(ediff-split-window-function (quote split-window-horizontally))
-    '(ediff-window-setup-function (quote ediff-setup-windows-plain)))
-
-(defalias 'list-buffers 'ibuffer)
-
-(use-package ace-window
+(use-package flyspell
 :ensure t
-:init
-(global-set-key [remap other-window] 'ace-window)
-(custom-set-faces
-'(aw-leading-char-face
-    ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-)
+:defer 1
+:diminish)
 
-(use-package eyebrowse
-:ensure t
+(use-package doom-themes
+:ensure t)
+
+(use-package undo-tree
+:ensure t)
+
+(use-package which-key
+:diminish which-key-mode
 :config
-(eyebrowse-mode t)
-(eyebrowse-setup-opinionated-keys))
-
-(use-package neotree
-:ensure t
-:bind ([f8] . neotree-toggle)
-:config
-(setq neo-default-system-application "open")
-(setq-default neo-show-hidden-files t))
-
-(use-package counsel
-  :ensure t
-)
-
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-(use-package swiper
-:ensure t
-:bind (("C-s" . swiper)
-    ("C-r" . swiper)
-    ("C-c C-r" . ivy-resume)
-    ("M-x" . counsel-M-x)
-    ("C-x C-f" . ido-find-file))
-:config
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t)
-    (setq ivy-display-style 'fancy)
-    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-    )
-
-(use-package auto-complete
-:ensure t
-:init
-(ac-config-default)
-(global-auto-complete-mode t)
-)
-
-(setq require-final-newline t)
-
-(use-package evil-multiedit
-:ensure t
-:config
-(evil-multiedit-default-keybinds))
-
-(use-package magit
-:ensure t
-:bind
-("C-x g" . magit-status))
+(which-key-mode))
 
 (setq-default show-trailing-whitespace t)
-
-;(require 'powerline)
-;(powerline-evil-vim-color-theme)
-(setq evil-normal-state-tag "NORMAL")
-(setq evil-insert-state-tag "INSERT")
-(setq evil-visual-state-tag "VISUAL")
--- configfiles/home/.emacs.d/init.el
+++ configfiles/home/.emacs.d/init.el
@@ -1,31 +1,30 @@
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-
-(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(org-babel-load-file (expand-file-name "~/.emacs.d/emacsinit.org"))
+(require 'org)
+(org-babel-load-file (expand-file-name (concat user-emacs-directory "emacsinit.org")))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (doom-molokai)))
+ '(ad-redefinition-action (quote accept))
+ '(custom-enabled-themes (quote (spacemacs-dark)))
  '(custom-safe-themes
    (quote
-    ("be9645aaa8c11f76a10bcf36aaf83f54f4587ced1b9b679b55639c87404e2499" default)))
+    ("56ffe2e4a924e2f5190055c8ef993fadba9a0329ece7be566d1a1b8e5a9c775b" "11e57648ab04915568e558b77541d0e94e69d09c9c54c06075938b6abc0189d8" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "78cb3874ba79a3ff53ba2e7aafc40e4567120848f947e3816464f3f01cf49aa1" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(ediff-diff-options "-w")
  '(ediff-split-window-function (quote split-window-horizontally))
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(neo-smart-open t t)
+ '(neo-theme (quote arrow) t)
  '(package-selected-packages
    (quote
-    (aggressive-indent smartparens evil-nerd-commenter highlight-parentheses rainbow-delimiters neotree ranger zenburn-theme which-key use-package try powerline ox-reveal orgalist org-bullets molokai-theme magit gnu-elpa-keyring-update eww-lnum evil doom-themes counsel auto-complete ace-window abyss-theme))))
+    (spacemacs-theme which-key toc-org smooth-scrolling rainbow-delimiters quelpa-use-package projectile paradox ox-reveal org-bullets neotree molokai-theme magit ido-completing-read+ htmlize eyebrowse evil-surround evil-org evil-numbers evil-nerd-commenter evil-multiedit evil-leader evil-ediff evil-cleverparens doom-themes doom-modeline counsel company auto-indent-mode auto-complete auto-compile ace-window)))
+ '(paradox-github-token t)
+ '(projectile-switch-project-action (quote neotree-projectile-action))
+ '(read-buffer-completion-ignore-case t)
+ '(read-file-name-completion-ignore-case t)
+ '(use-package-always-ensure t)
+ '(use-package-verbose nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
