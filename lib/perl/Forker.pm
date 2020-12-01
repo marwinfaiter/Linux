7b8ef2110f5de37bdc66b5f3a6b9e6ef4d472118
marwinfaiter <59771320+marwinfaiter@users.noreply.github.com>
2020-12-01 13:29:54 +0100

    Forker is now moose based.

--- lib/perl/Forker.pm
+++ lib/perl/Forker.pm
@@ -1,81 +1,153 @@
 package Forker;
 
-# Author: Martin Falkmer, mf881899
+use Moose;
+use namespace::autoclean;
 
-use strict;
 use Data::Dumper;
+use Storable ();
+use File::Spec;
+
+has 'kids'        => ( is => 'rw', isa => 'ArrayRef', default => sub {[]} );
+has 'threads'     => ( is => 'rw', isa => 'Int' );
+has 'file_handle' => ( is => 'rw' );
+has 'wait_print'   => ( is => 'rw', isa => 'Bool', default => 0 );
+has 'no_print'   => ( is => 'rw', isa => 'Bool', default => 0 );
+has 'parent_pid'  => ( is => 'rw', isa => 'Int', default => sub {$$});
 
 $SIG{CHLD} = 'IGNORE';
 
-sub new {
+sub add_fork {
+    my ($self, $fork_id, $code, @args) = @_;
+
+    if ( $self->threads ) {
+        while ( $self->running >= $self->threads ) {
+            select undef, undef, undef, 0.5;
+        }
+    }
+
+    $self->_print_fork();
+
+    $fork_id ||= scalar @{$self->kids};
+    push @{$self->kids}, { fork_id => $fork_id };
+
+    my $pid = open $self->kids->[-1]->{output}, "-|";
+
+    if (!defined $pid) {
+        $self->logger->log(fatal => "Failed to fork");
+        shift @{$self->kids};
+    }
+
+    if ($pid) {
+        $self->kids->[-1]->{pid} = $pid;
+        return $pid;
+    }
+
+    if (ref $code eq "CODE") {
+        my $data = $code->(@args);
+
+        $self->exit_child($data);
+    }
+}
+
+sub exit_child {
+    my ($self, $return_data) = @_;
+
+    $self->_store($return_data);
+
+    exit;
+}
+
+sub finish {
     my $self = shift;
 
-    my %args = (
-        kids        => [],
-        pids        => [],
-        output      => [],
-        threads     => undef,
-        filehandle  => undef,
-        save_output => undef,
-        waitprint   => undef,
-        @_,
-    );
+    foreach my $kid (@{$self->kids}) {
+        waitpid $kid->{pid}, 0;
 
-    return bless \%args, $self;
-}
+        $self->_print($kid);
 
-sub AddFork {
-    no warnings "exiting";
-    my ($self, $fork) = @_;
-
-    if ($self->{threads} && scalar(grep {kill(0,$_)} @{$self->{pids}}) >= $self->{threads}) {
-        sleep 1 until scalar(grep {kill(0,$_)} @{$self->{pids}}) < $self->{threads};
+        delete $kid->{printed};
     }
 
-    if (!$self->{waitprint}) {
-        $self->_PrintFork();
-    }
-
-    push @{$self->{kids}}, {
-        'fork' => $fork || ""
-    };
-
-    defined(my $pid = open $self->{kids}->[$#{$self->{kids}}]->{pid}, "-|") or print "Failed to fork\n" and pop @{$self->{kids}} and next;
-    push @{$self->{pids}}, $pid and next if $pid;
-}
-
-sub _PrintFork {
-    my $self = shift;
-
-    while (scalar @{$self->{kids}}) {
-        last if kill(0,$self->{pids}->[0]);
-        my $pid = shift @{$self->{kids}};
-        shift @{$self->{pids}};
-        $self->_Print($pid);
-    }
-}
-
-sub _Print {
-    my ($self, $pid) = @_;
-
-    if ($self->{save_output}) {
-        my $output = $pid->{pid};
-        $pid->{pid} = <$output>;
-        push @{$self->{output}}, $pid;
+    if (scalar @{$self->kids} == 1) {
+        return $self->kids->[0];
     }
     else {
-        $pid = $pid->{pid};
-        print { $self->{filehandle} || *STDOUT } <$pid>;
+        return $self->kids;
     }
 }
 
-sub Finish {
+sub running {
     my $self = shift;
 
-    while (my $pid = shift @{$self->{kids}}) {
-        waitpid 0, $pid->{pid};
-        $self->_Print($pid);
+    my @running = grep { kill(0, $_->{pid}) } @{$self->kids};
+
+    return wantarray ? @running : scalar @running;
+}
+
+sub _store {
+    my ($self, $return_data) = @_;
+
+    return unless $return_data;
+
+    my $storable_tempfile = File::Spec->catfile('/tmp', 'Features-Forker-' . $self->{parent_pid} . '-' . $$ . '.txt');
+    my $stored = eval { return Storable::store($return_data, $storable_tempfile); };
+
+    # handle Storables errors, IE logcarp or carp returning undef, or die (via logcroak or croak)
+    if (not $stored or $@) {
+        warn(qq|The storable module was unable to store the child's data structure to the temp file "$storable_tempfile":  | . join(', ', $@));
     }
 }
+
+sub _retrieve {
+    my ($self, $kid) = @_;
+
+    my $retrieved = undef;
+
+    my $storable_tempfile = File::Spec->catfile('/tmp', 'Features-Forker-' . $self->{parent_pid} . '-' . $kid . '.txt');
+
+    if (-e $storable_tempfile) {  # child has option of not storing anything, so we need to see if it did or not
+        $retrieved = eval { Storable::retrieve($storable_tempfile) };
+
+        # handle Storables errors
+        if (not $retrieved or $@) {
+            warn(qq|The storable module was unable to retrieve the child's data structure from the temporary file "$storable_tempfile":  | . join(', ', $@));
+        }
+
+        # clean up after ourselves
+        unlink $storable_tempfile;
+    }
+
+    return $retrieved;
+}
+
+sub _print_fork {
+    my $self = shift;
+
+    return if $self->wait_print;
+
+    foreach my $kid (@{$self->kids}) {
+        last if kill(0, $kid->{pid});
+
+        $self->_print($kid);
+    }
+}
+
+sub _print {
+    my ($self, $kid) = @_;
+
+    return if (ref $kid->{output} eq "GLOB");
+
+    my $output = $kid->{output};
+    $kid->{output} =  join("", <$output>);
+
+    my $return_data = $self->_retrieve($kid->{pid});
+    $kid->{data} = $return_data;
+
+    if (!$self->no_print) {
+        print { $self->file_handle || *STDOUT } $kid->{output};
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
